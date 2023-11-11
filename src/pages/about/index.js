649e703312c092da84ec33da0798200ecca0c0bd
paperhsiaooo <paper.hsiaooo@gmail.com>
2023-11-11 14:08:27 +0800

    feat: init project

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -5,10 +5,15 @@ function About() {
     const handleEmailClick = () => {
         console.log('handleEmailClick!');
     }
+    
+    const handleCancelClick = () => {
+        console.log('handleEmailClick!');
+    }
 
     return (
         <Stack>
-            <Button variant='contained' onClick={handleEmailClick}>送出 API</Button>
+            <Button variant='contained' onClick={handleEmailClick}>送出信件</Button>
+            <Button variant='contained' onClick={handleCancelClick}>取消</Button>
         </Stack>
     )
 }
